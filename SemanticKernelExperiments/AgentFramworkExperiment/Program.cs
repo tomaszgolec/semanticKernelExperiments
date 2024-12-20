dd847e875ba57776482250e378dd098a1e0eef2f
DESKTOP-KBE0IFI\Tomasz <tomaszgolec1993@gmail.com>
2024-12-20 16:01:59 +0100

    initial commit

---
+++ SemanticKernelExperiments/AgentFramworkExperiment/Program.cs
@@ -0,0 +1,4 @@
+﻿//SEMANTIC KERNEL AGENT FRAMEWORK
+//Experiment based on this part of documentation 
+//https://learn.microsoft.com/en-us/semantic-kernel/frameworks/agent/?pivots=programming-language-csharp
+Console.WriteLine("Hello, World!");
