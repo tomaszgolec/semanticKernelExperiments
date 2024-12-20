7fdd582b7f27f557a58d13614a4e072d4694a1c1
DESKTOP-KBE0IFI\Tomasz <tomaszgolec1993@gmail.com>
2024-12-20 17:19:04 +0100

    getting started project plus simple api keys manager

---
+++ SemanticKernelExperiments/ApiKeysManager/ApiKeysManager.cs
@@ -0,0 +1,25 @@
+﻿
+using Microsoft.Extensions.Configuration;
+
+public class ApiKeysManager
+{
+    private static readonly Lazy<ApiKeysManager> _instance =
+        new Lazy<ApiKeysManager>(() => new ApiKeysManager());
+    private readonly IConfigurationRoot _config;
+
+
+    private ApiKeysManager()
+    {
+
+        _config = new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: true, reloadOnChange: true).Build();
+    }
+
+    public static ApiKeysManager Instance => _instance.Value;
+
+    public string GetApiKey(string keyName)
+    {
+        var key = _config[$"AppSettings:{keyName}"];
+        return key ?? throw new KeyNotFoundException($"API Key: {projectName} is not found.");
+    }
+}
+
---
+++ SemanticKernelExperiments/GettingStarted/Program.cs
@@ -0,0 +1,56 @@
+﻿// Import packages
+using GettingStarted.Pluggins;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+// Populate values from your OpenAI deployment
+var modelId = "gpt-3.5-turbo";
+var apiKey = ApiKeysManager.Instance.GetApiKey("OpenApiKey");
+// Create a kernel with Azure OpenAI chat completion
+var builder = Kernel.CreateBuilder().AddOpenAIChatCompletion(modelId, apiKey);
+
+// Add enterprise components
+builder.Services.AddLogging(services => services.AddConsole().SetMinimumLevel(LogLevel.Trace));
+
+// Build the kernel
+Kernel kernel = builder.Build();
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+// Add a plugin (the LightsPlugin class is defined below)
+kernel.Plugins.AddFromType<LightsPlugin>("Lights");
+
+// Enable planning
+OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
+{
+    FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
+};
+
+// Create a history store the conversation
+var history = new ChatHistory();
+
+// Initiate a back-and-forth chat
+string? userInput;
+do
+{
+    // Collect user input
+    Console.Write("User > ");
+    userInput = Console.ReadLine();
+
+    // Add user input
+    history.AddUserMessage(userInput);
+
+    // Get the response from the AI
+    var result = await chatCompletionService.GetChatMessageContentAsync(
+        history,
+        executionSettings: openAIPromptExecutionSettings,
+        kernel: kernel);
+
+    // Print the results
+    Console.WriteLine("Assistant > " + result);
+
+    // Add the message from the agent to the chat history
+    history.AddMessage(result.Role, result.Content ?? string.Empty);
+} while (userInput is not null);
