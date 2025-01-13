be2c719649e8f5c987cad36825cd6063f478c5a3
DESKTOP-KBE0IFI\Tomasz <tomaszgolec1993@gmail.com>
2025-01-13 21:10:20 +0100

    further expermients

---
+++ SemanticKernelExperiments/AIParserTestApp/ExampleModel.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.SemanticKernel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIParserTestApp
+{
+    public class ExampleModel
+    {
+        [KernelFunction("create_examplemodel")]
+        [Description("Create instnace of ExampleModel class")]
+        [return: Description("An instance of ExampleModel class")]
+        public ExampleModel CreateExampleModel (ExampleModel model)
+        {
+            return model;
+        }
+
+
+        public string User { get; set; }
+        public string Name { get; set; }
+        public Options Options { get; set; }
+        public bool Active { get; set; }
+    }
+
+    public class Options
+    {
+        public string Language { get; set; }
+        public string Theme { get; set; }
+    }
+}
--- SemanticKernelExperiments/AIParserTestApp/Program.cs
+++ SemanticKernelExperiments/AIParserTestApp/Program.cs
@@ -1,15 +1,27 @@
-﻿// Import packages
+﻿using AIParserTestApp;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
 
+string exampleJson = @"
+{
+    ""userId"": ""12345"",
+    ""name"": ""John Doe"",
+    ""preferences"": {
+        ""lang"": ""English"",
+        ""theme"": ""dark""
+    },
+    ""isActive"": true
+}";
 
+//TODO jesli nei da sie odczytac tego tak po prostu z wyniku 
+//to moze sprobuj wrzucic mu klase po refleksji zbey po prostu dopasowal jsona tak 
+//zeby pasowal do obiektu
 
 var modelId = "gpt-3.5-turbo";
 var apiKey = ApiKeysManager.Instance.GetApiKey("OpenApiKey");
-
 
 var builder = Kernel.CreateBuilder().AddOpenAIChatCompletion(modelId, apiKey);
 
@@ -17,6 +29,8 @@ builder.Services.AddLogging(services => services.AddConsole().SetMinimumLevel(Lo
 
 Kernel kernel = builder.Build();
 var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+kernel.Plugins.AddFromType<ExampleModel>("ExampleModel");
 
 OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
 {
@@ -26,19 +40,18 @@ OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
 var history = new ChatHistory();
 
 string? userInput;
-do
-{
-    Console.Write("User > ");
-    userInput = Console.ReadLine();
 
-    history.AddUserMessage(userInput);
+Console.Write("User > ");
+userInput = $"Sparsuj nastepujacy json do klasy ExampleModel :{exampleJson}";
 
-    var result = await chatCompletionService.GetChatMessageContentAsync(
-        history,
-        executionSettings: openAIPromptExecutionSettings,
-        kernel: kernel);
-    
-    Console.WriteLine("Assistant > " + result);
+history.AddUserMessage(userInput);
 
-    history.AddMessage(result.Role, result.Content ?? string.Empty);
-} while (userInput is not null);
+var result = await chatCompletionService.GetChatMessageContentAsync(
+    history,
+    executionSettings: openAIPromptExecutionSettings,
+    kernel: kernel);
+
+
+Console.WriteLine("Assistant > " + result);
+
+history.AddMessage(result.Role, result.Content ?? string.Empty);
