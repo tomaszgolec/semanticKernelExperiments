485833b26a037bbab1faac7de87afe4d7181c963
T.Golec <tomasz.golec.external@worldline.com>
2025-01-14 14:01:27 +0100

    first ai parser prototype

--- SemanticKernelExperiments/AIParserTestApp/Program.cs
+++ SemanticKernelExperiments/AIParserTestApp/Program.cs
@@ -4,7 +4,8 @@ using Microsoft.SemanticKernel.ChatCompletion;
 
 
 var apiKey = ApiKeysManager.Instance.GetApiKey("OpenApiKey");
-string exampleJson = @"
+var llmModel = "gpt-3.5-turbo";
+    string exampleJson = @"
 {
     ""userId"": ""12345"",
     ""name"": ""John Doe"",
@@ -15,40 +16,8 @@ string exampleJson = @"
     ""isActive"": true
 }";
 
-IKernelBuilder builder = Kernel.CreateBuilder();
-builder.AddOpenAIChatCompletion("gpt-3.5-turbo", apiKey);
-builder.Plugins.AddFromType<ExampleModel>();
+var aiParser = new AiParserPrototype(llmModel, apiKey);
+var model2 = await aiParser.NoGenericParse(exampleJson);
 
-Kernel kernel = builder.Build();
+Console.WriteLine(model2.ToString());
 
-IChatCompletionService chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
-
-// Manual function invocation needs to be enabled explicitly by setting autoInvoke to false.
-PromptExecutionSettings settings = new() { FunctionChoiceBehavior = Microsoft.SemanticKernel.FunctionChoiceBehavior.Required(autoInvoke: false) };
-
-ChatHistory chatHistory = [];
-chatHistory.AddUserMessage(exampleJson);
-
-ChatMessageContent result = await chatCompletionService.GetChatMessageContentAsync(chatHistory, settings, kernel);
-
-// Check if the AI model has generated a response.
-if (result.Content is not null)
-{
-    //throw expetion here that somehow model gerenate response before usage of a tool
-}
-
-chatHistory.Add(result);
-
-// Check if the AI model has chosen any function for invocation.
-IEnumerable<FunctionCallContent> functionCalls = FunctionCallContent.GetFunctionCalls(result);
-if (!functionCalls.Any())
-{
-    //throw Exception that that plugin is missing 
-}
-
-var functionCall = functionCalls.FirstOrDefault();
-// Invoking the function
-FunctionResultContent resultContent = await functionCall.InvokeAsync(kernel);
-ExampleModel model = (ExampleModel)resultContent.Result;
-
-Console.ReadKey();
