1410e2f3a572752e4f6e50f1966009259b52e244
T.Golec <tomasz.golec.external@worldline.com>
2025-01-14 14:47:03 +0100

    cleanup

--- SemanticKernelExperiments/AIParserTestApp/AiSerializerPrototype.cs
+++ SemanticKernelExperiments/AIParserTestApp/AiSerializerPrototype.cs
@@ -15,64 +15,18 @@ namespace AIParserTestApp
             _apiKey = apiKey;
         }
 
-        public async Task<T> Parse<T>(string json)
-        {
-
-            IKernelBuilder builder = Kernel.CreateBuilder();
-            builder.AddOpenAIChatCompletion(_model, _apiKey);
-            //add type ass pareameter to make it generic 
-            builder.Plugins.AddFromType<T>();
-
-            Kernel kernel = builder.Build();
-
-            IChatCompletionService chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
-
-            // Manual function invocation needs to be enabled explicitly by setting autoInvoke to false.
-            PromptExecutionSettings settings = new() { FunctionChoiceBehavior = Microsoft.SemanticKernel.FunctionChoiceBehavior.Required(autoInvoke: false) };
-
-            ChatHistory chatHistory = [];
-            chatHistory.AddUserMessage(json);
-
-            ChatMessageContent result = await chatCompletionService.GetChatMessageContentAsync(chatHistory, settings, kernel);
-
-            if (result.Content is not null)
-            {
-                //throw expetion here that somehow model gerenate response before usage of a tool
-            }
-
-            chatHistory.Add(result);
-
-            // Check if the AI model has chosen any function for invocation.
-            IEnumerable<FunctionCallContent> functionCalls = FunctionCallContent.GetFunctionCalls(result);
-            if (!functionCalls.Any())
-            {
-                //throw Exception that that plugin is missing 
-            }
-
-            var functionCall = functionCalls.FirstOrDefault();
-            // Invoking the function
-            FunctionResultContent resultContent = await functionCall.InvokeAsync(kernel);
-            T model = (T)resultContent.Result;
-
-            return model;
-        }
-
-
         public async Task<T> Deserialize<T>(string json)
         {
 
             IKernelBuilder builder = Kernel.CreateBuilder();
             builder.AddOpenAIChatCompletion(_model, _apiKey);
-            //add type ass pareameter to make it generic 
-
 
             builder.Plugins.AddFromType<ModelCreatorPlugin<T>>();
 
             Kernel kernel = builder.Build();
 
             IChatCompletionService chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
-
-            // Manual function invocation needs to be enabled explicitly by setting autoInvoke to false.
+            
             PromptExecutionSettings settings = new() { FunctionChoiceBehavior = Microsoft.SemanticKernel.FunctionChoiceBehavior.Required(autoInvoke: false) };
 
             ChatHistory chatHistory = [];
@@ -87,7 +41,6 @@ namespace AIParserTestApp
 
             chatHistory.Add(result);
 
-            // Check if the AI model has chosen any function for invocation.
             IEnumerable<FunctionCallContent> functionCalls = FunctionCallContent.GetFunctionCalls(result);
             if (!functionCalls.Any())
             {
@@ -95,7 +48,7 @@ namespace AIParserTestApp
             }
 
             var functionCall = functionCalls.FirstOrDefault();
-            // Invoking the function
+            
             FunctionResultContent resultContent = await functionCall.InvokeAsync(kernel);
             T model = (T)resultContent.Result;
 
